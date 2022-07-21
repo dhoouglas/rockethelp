40a07bbc5558d9e3a771a6a4370fd798d9f2fe27
unknown <dhoouglas@hotmail.com>
2022-07-20 23:28:28 -0300

    Tela de SingIn created

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
+
 import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn () {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     const { colors } = useTheme();
+
+    function handleSignIn(){
+        console.log(name,password);
+    }
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
@@ -16,15 +25,20 @@ export function SignIn () {
             </Heading>
 
             <Input 
-                placeholder="E-mail"
-                mb={4}    
+                mb={4} 
+                placeholder="E-mail"   
                 InputLeftElement={<Icon as ={ <Envelope color={colors.gray[300]}/> }  ml={4} />}
+                onChangeText={setName}
             />
             <Input 
+                mb={8} 
                 placeholder="Senha"
                 InputLeftElement={<Icon as ={ <Key color={colors.gray[300]}/> }  ml={4} />}
                 secureTextEntry
+                onChangeText={setPassword}
             />
+
+            <Button title="Entrar" w="full" onPress={handleSignIn}/>
 
         </VStack>
     )
