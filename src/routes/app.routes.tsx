340660ff17d0017fa5d13471884e7f324d243cbd
unknown <dhoouglas@hotmail.com>
2022-07-25 22:38:52 -0300

    Install navigation-Stack and config routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,7 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { Home } from "../screens/Home";
+import { Details } from "../screens/Details";
+import { Register } from "../screens/Register";
+
+const { Navigator, Screen} = createNativeStackNavigator(); 
