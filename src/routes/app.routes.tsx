8190e996646c6bc5df83aca857b4558f36acb153
unknown <dhoouglas@hotmail.com>
2022-07-25 22:53:25 -0300

    Routes created

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,3 +5,13 @@ import { Details } from "../screens/Details";
 import { Register } from "../screens/Register";
 
 const { Navigator, Screen} = createNativeStackNavigator(); 
+
+export function AppRoutes(){
+    return(
+        <Navigator screenOptions={{ headerShown:false }}>
+            <Screen name="home " component={Home} />
+            <Screen name="new" component={Register} />
+            <Screen name="details" component={Details} />
+        </Navigator>
+    )
+}
