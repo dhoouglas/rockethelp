a00a8f3b77dc2ec579a1801bf2689a010a359ab8
unknown <dhoouglas@hotmail.com>
2022-07-25 23:21:08 -0300

    Navigation Configured

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            hone: undefined;
+            new: undefined;
+            details: { orderId: string };
+        }
+    }
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 
@@ -7,15 +8,29 @@ import Logo from '../assets/logo_secondary.svg';
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
-import { color } from 'native-base/lib/typescript/theme/styled-system';
+
 
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
     const [orders, setOrders] = useState<OrderProps[]>([
-        
+        {
+            id: '456',
+            patrimony: '123456',
+            when: '18/07/2022 às 10:00',
+            status: 'open'
+        }
     ]);
 
+    const navigation = useNavigation();
     const { colors } = useTheme();
+
+    function handleNewOrder() {
+        navigation.navigate('new');
+    }
+
+    function handleOpenDetails(orderId: string){
+        navigation.navigate('details', { orderId })
+    }
 
     return (
         <VStack flex={1} pb={6} bg="gray.700">
@@ -40,11 +55,11 @@ export function Home() {
             <VStack flex={1} px={6}>
                 <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
                     <Heading color="gray.100">
-                        Meus chamados
+                        Solicitações
                     </Heading>
 
                     <Text color="gray.200">
-                        3
+                        {orders.length}
                     </Text>
                 </HStack>
 
@@ -67,7 +82,7 @@ export function Home() {
                 <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
-                    renderItem={({ item }) => <Order data={item} />}
+                    renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
                     showsVerticalScrollIndicator={false}
                     contentContainerStyle={{ paddingBottom: 100 }}
                     ListEmptyComponent={() => (
@@ -82,10 +97,9 @@ export function Home() {
                     )}
                 />
 
-                <Button title="Nova solicitação" />
+                <Button title="Nova solicitação" onPress={handleNewOrder} />
 
             </VStack>
-
 
         </VStack>
     );
