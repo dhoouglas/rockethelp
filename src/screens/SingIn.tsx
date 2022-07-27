aa90e6c4b9e77f92e02403cbf400cb1244f6bf50
unknown <dhoouglas@hotmail.com>
2022-07-27 20:36:35 -0300

    add and config local.properties

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
@@ -9,13 +10,18 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn () {
-    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const { colors } = useTheme();
 
     function handleSignIn(){
-        console.log(name,password);
+        if (!email || !password) {
+            return Alert.alert('Entrar', 'Informe e-mail e senha.');
+        }
+
+
+        
     }
     
     return (
@@ -30,7 +36,7 @@ export function SignIn () {
                 mb={4} 
                 placeholder="E-mail"   
                 InputLeftElement={<Icon as ={ <Envelope color={colors.gray[300]}/> }  ml={4} />}
-                onChangeText={setName}
+                onChangeText={setEmail}
             />
             <Input 
                 mb={8} 
